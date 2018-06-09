9b0f4400cff8ea7240868d99157c8d5bf6903650
Leonard Siebeneicher <leonard7e@gmail.com>
2018-06-10 00:50:38 +0200

    Das Lesen eines Spielfeldes lässt sich auch direkt aus der Datei umsetzen. Dieser Ansatz wird jetzt verfolgt.

--- spielfeld.c
+++ spielfeld.c
@@ -1,6 +1,7 @@
 #include "spielfeld.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 /*
     Ein Spielfeld besteht aus einem Zeiger auf ein Terrain-terrain_buffer, einem Einstiegspunkt für den Spieler, und Informationen über Breite und Höhe des Terrain-Buffers.
@@ -33,34 +34,27 @@ struct SpielFeld *lade_spielfeld (
     }
 
     /*
-        Das Lesen der Spielfeld-Datei geschieht Zeilenweise. Das Ende jeder Zeile wird von einem Zeilenumbruch festgelegt.
-    */
-    char *line_string = NULL; // Gelesene Zeile, als String
-    size_t line_length = 0; // Länge der gelesenen Zeile
-    ssize_t read_length = 0; // Zum ermitteln, ob das Ende der Datei erreicht wurde.
-
-    /*
+        Das Lesen der ersten beiden Abschnitte der Spielfeld-Datei geschieht Zeilenweise. Das Ende jeder Zeile wird von einem Zeilenumbruch festgelegt.
         Die erste Zeile beinhaltet den Abschnitt:
             Spielfeld <breite> <hoehe>
     */
-    getline(&line_string, &line_length, felddatei);
     uint breite=0;
     uint hoehe=0;
-    sscanf(line_string, "SpielFeld %d %d", &breite, &hoehe );
+    fscanf(felddatei, "SpielFeld %d %d\n", &breite, &hoehe );
 
     /*
         Der nächste Abschnitt wäre:
             Spieler <einstieg_x> <einstieg_y>
     */
-    getline(&line_string, &line_length, felddatei);
     uint p_x=0;
     uint p_y=0;
-    sscanf(line_string, "Spieler %d %d", &p_x, &p_y );
+    fscanf(felddatei, "Spieler %d %d\n", &p_x, &p_y );
 
     /*
         Zum letzten Abschnitt. Der Inhalt des Terrain-Buffers wird geladen.
 
-        Die Addresse des Terrain-Buffers wird durch */
+        Die Addresse des Terrain-Buffers wird durch
+    */
     unsigned int *terrain_buffer = malloc(breite*hoehe*sizeof(uint));
     /*
         festgehalten. Beim Zugreifen und Verändern des Terrain-Buffers wird die Variable terrain_buffer allerdings nicht verändert. Für Veränderungen am Buffer führen wir die Variable bcursor ein.
@@ -68,21 +62,24 @@ struct SpielFeld *lade_spielfeld (
     unsigned int *bcursor = terrain_buffer;
 
     /*
-        Wir sind soweit, daß wir den Terrain-Buffer lesen können. Wie oben beschrieben, tun wir dies Zeile für Zeile.
+        Wir sind soweit, daß wir den Terrain-Buffer lesen können. Die Variable ...
     */
-    while ((read_length = getline(&line_string, &line_length, felddatei)) != -1) {
+    char terrain;
+    /*
+        wird benutzt, um gelesene Terraininformationen abzuspeichern.
+    */
+    while ((terrain = fgetc(felddatei)) != -1) {
         /*
-            Die gelesene Zeile, welche in line_string festgehalten wird, wird in der nächsten Schleife in ihre einzelnen Felder zerlegt.
+            Das Terrain-Buffer wird Zeichen für Zeichen gelesen. Dabei werden Leerzeichen ignoriert.
         */
-        for (int i=0; i<read_length-1; i+=1) {
-            *bcursor = line_string[i];
+        if ( !isspace(terrain) ) {
+            *bcursor = terrain;
             bcursor ++; // Den bcursor um ein Feld verschieben.
         }
     }
 
     // Diese Beiden brauchen wir nicht mehr.
     bcursor = NULL;
-    free(line_string);
 
     /*
         Nun haben wir alles, was wir für die Zusammensetzung der Struktur SpielFeld brauchen. Wir können sie zusammengeseten.
